65a71cd7acee7f22197b69caf725f9065a62db90
sharee-tech <84551254+sharee-tech@users.noreply.github.com>
2023-08-03 19:55:04 -0500

    added Description.js component

---
+++ part1/studio/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import RecipeDescription from "./components/Description";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <RecipeDescription />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ part1/studio/src/components/Description.js
@@ -0,0 +1,39 @@
+import styles from "./Description.module.css";
+import React from "react";
+
+class RecipeDescription extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <h1>Recipe Title</h1>
+          <p>Short recipe description</p>
+        </div>
+        <RecipeAuthor />
+      </div>
+    );
+  }
+}
+
+function RecipeAuthor() {
+  const authorLink = "https://organicallyaddison.com/about/";
+  const authorPhoto =
+    "https://organicallyaddison.com/wp-content/uploads/2023/05/WHITE-DRESS-scaled.jpg";
+  const authorName = "Addison";
+
+  return (
+    <div className={styles.recipeAuthorBlock}>
+      <img
+        src={authorPhoto}
+        alt="Picture of Organically Addison"
+        className={styles.imageUpdates}
+      />
+      <div>
+        <h3>{authorName}</h3>
+        <a href={authorLink}>About Addison</a>
+      </div>
+    </div>
+  );
+}
+
+export default RecipeDescription;
